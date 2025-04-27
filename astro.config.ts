dff6410090715620ee8facb597d555b862e9a8ca
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2025-04-27 09:44:44 -0400

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
       short_name: "Hanzi Lanterns",
       description: siteConfig.description,
       lang: siteConfig.lang,
-      icon: "public/images/seal.jpg",
+      icon: "public/seal.jpg",
       icons: [
         {
           src: "icons/apple-touch-icon.png",
